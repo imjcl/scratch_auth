8613753396194c5543b41f3cb23a74f516c51b60
James Liu <jaycliu@gmail.com>
2014-10-09 21:28:48 -0700

    create routes for edit page and edit view template, does not execute any action currently

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def destroy
     User.find(params[:id]).remove
 
