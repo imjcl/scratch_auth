8a2ecab69bd2f7159de5c2a8b8b3bc151b1c507e
James Liu <jaycliu@gmail.com>
2014-10-09 21:04:26 -0700

    created a seed for a default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+default = User.new(email: 'default@localhost', password: 'test')
+default.save
