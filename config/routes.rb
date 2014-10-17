1b785ae4bbde5ee4d071a56df5d9716f03a5572e
James Liu <jaycliu@gmail.com>
2014-10-16 17:40:10 -0700

    added matching routes for existing posts actions

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   get '/log-in' => 'sessions#new'
   post '/log-in' => 'sessions#create'
   get '/log-out' => 'sessions#destroy', as: :log_out
+
+  get '/posts' => 'posts#index'
+  post '/posts' => 'posts#create'
+  get '/posts/new' => 'posts#new'
+  get '/posts/:title' => 'posts#show', as: :post
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
