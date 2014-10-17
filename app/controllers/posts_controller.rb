c6a247c5621c5fca9809449afc8d6f359a7a6caf
James Liu <jaycliu@gmail.com>
2014-10-16 17:43:57 -0700

    fill in actions in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,24 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @user.save
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
   end
 
   def show
+    @post = Post.where(title: params[:title])
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
 end
