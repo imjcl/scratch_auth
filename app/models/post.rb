282db6b2bfb145cdb85f3c4cecb6b827be156925
James Liu <jaycliu@gmail.com>
2014-10-16 20:48:34 -0700

    tie creating and updating to a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,14 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps
-  before_create :create_slug
+  before_create :create_slug, :assign_creator
+  before_update :assign_updator
 
   field :title, type: String
   field :body, type: String
   field :slug, type: String
+  field :created_by, type: String
+  field :updated_by, type: String
 
   validates_presence_of :title
 
@@ -14,4 +17,13 @@ class Post
   def create_slug
     self.slug = self.title.downcase.gsub(/\s/, '-').gsub(/[^0-9A-Za-z\-]/, '')
   end
+
+  def assign_creator
+    self.created_by = self.user_id
+  end
+
+  def assign_updator
+    self.updated_by = self.user_id
+  end
+
 end
