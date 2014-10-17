10e6e5cd70c7edbbb105e60570ada66d2a8c0491
James Liu <jaycliu@gmail.com>
2014-10-16 18:00:36 -0700

    added slug creation into posts for reference

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,17 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps
+  before_create :create_slug
 
   field :title, type: String
   field :body, type: String
+  field :slug, type: String
 
   validates_presence_of :title
 
   belongs_to :user
+
+  def create_slug
+    self.slug = self.title.downcase.gsub(/\s/, '-').gsub(/[^0-9A-Za-z\-]/, '')
+  end
 end
